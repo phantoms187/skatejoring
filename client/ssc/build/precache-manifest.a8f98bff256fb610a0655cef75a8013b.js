60a3208f01a42521a7fc1e3be738598e225b5d75
phantoms187 <dwd2@pdx.edu>
2019-09-17 18:01:22 -0700

    From atom

---
+++ client/ssc/build/precache-manifest.a8f98bff256fb610a0655cef75a8013b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245ebbe6cf86a97b128cc79f3ecf243b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99e59f17c622377031d4",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "0ecb31ee7d1e4a1a7bc7",
+    "url": "/static/css/main.bf2d2499.chunk.css"
+  },
+  {
+    "revision": "99e59f17c622377031d4",
+    "url": "/static/js/2.92047fb7.chunk.js"
+  },
+  {
+    "revision": "0ecb31ee7d1e4a1a7bc7",
+    "url": "/static/js/main.76f47df3.chunk.js"
+  },
+  {
+    "revision": "06b7cfbe5a7458d38821",
+    "url": "/static/js/runtime~main.1d872ddf.js"
+  }
+]);
