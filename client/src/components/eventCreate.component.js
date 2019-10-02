29736d32e116f45fa2cb0369bcd8d5e247f5dc84
phantoms187 <warrend187@gmail.com>
2019-10-02 08:39:42 +0000

    Heroku 10/01 auth working

---
+++ client/src/components/eventCreate.component.js
@@ -0,0 +1,241 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import moment from 'moment';
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { addEvent } from "../actions/eventActions";
+import { withRouter } from "react-router-dom";
+const isEmpty = require("is-empty");
+
+
+
+//Class to create a new user for weather distribution
+class CreateEvent extends Component {
+  constructor() {
+      super();
+      
+      //Set current state to empty
+      this.state = {
+          name: '',
+          date: '',
+          time: '',
+          locationStreet: '',
+          locationCity: '',
+          locationState: '',
+          locationZip: '',
+          errors: {}
+      };
+  }
+  
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({
+        errors: nextProps.errors
+      });
+    }
+  }
+  
+  //When input for each is changed, set new state of variable
+  onChange = e => {
+    this.setState({ [e.target.id]: e.target.value });
+  }
+  
+  
+
+//When the form is submitted, set object to post with axios
+  onSubmit = e => {
+    e.preventDefault();
+    
+          const place = {
+            name: !isEmpty(this.state.name) ? this.state.name : "",
+            street: !isEmpty(this.state.locationStreet) ? this.state.locationStreet : "",
+            city: this.state.locationCity,
+            state: this.state.locationState,
+            zip: !isEmpty(this.state.locationZip) ? this.state.locationZip : "",
+            date: moment(this.state.date, 'YYYY-MM-DD').format('dddd, MMMM Do YYYY'),
+            time: moment(this.state.time, 'HH:mm a').format('h:mm A'),
+          };
+    
+    axios.post('https://35a7deb5f76e493e9412648419a0a663.vfs.cloud9.us-west-2.amazonaws.com/weather', place)
+          .then(response => {
+              this.setState({ 
+                    weather: response.data.currently.summary, 
+                    icon: (response.data.currently.icon).toUpperCase().replace(/-/g,'_'),
+                });
+          })
+          .then( () =>{
+            
+            const event = {
+              name: this.state.name,
+              date: moment(this.state.date, 'YYYY-MM-DD').format('dddd, MMMM Do YYYY'),
+              time: moment(this.state.time, 'HH:mm a').format('h:mm A'),
+              location: this.state.locationStreet + " " + this.state.locationCity + ", " + this.state.locationState + " " + this.state.locationZip,
+              weather: this.state.weather,
+              icon: this.state.icon
+            };
+           
+            this.props.addEvent(event, this.props.history);
+            
+          })
+          .catch((error) => {
+              console.log(error);
+          });
+  }
+  
+ //Render form for create 
+  render() {
+    return (
+        <div class="title-welcome container">
+          <div class="row">
+            <div class="col-md-8 col-lg-6 offset-lg-3">
+              <h3>Add New Event</h3>
+              <form onSubmit={this.onSubmit}>
+              <div className="form-group">
+                      <label>Event Name:  </label>
+                      <input 
+                        type="text"
+                        id = "name"
+                        className="form-control" 
+                        value={this.state.name}
+                        onChange={this.onChange}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>Date:  </label>
+                      <input 
+                        type="date"
+                        id = "date"
+                        className="form-control" 
+                        value={this.state.date}
+                        onChange={this.onChange}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>Time:  </label>
+                      <input 
+                        type="time" 
+                        id = "time"
+                        className="form-control" 
+                        value={this.state.time}
+                        onChange={this.onChange}
+                        />
+                  </div>
+                   <div className="form-group">
+                      <label>Address: </label>
+                      <input 
+                        type="text"
+                        id = "locationStreet"
+                        className="form-control"
+                        value={this.state.locationStreet}
+                        onChange={this.onChange}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>City: </label>
+                      <input 
+                        type="text"
+                        id = "locationCity"
+                        className="form-control"
+                        value={this.state.locationCity}
+                        onChange={this.onChange}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <label>State: </label>
+                      <select value={this.state.locationState} id = "locationState" onChange={this.onChange} className="form-control">
+                        <option value="AL">Alabama</option>
+                      	<option value="AK">Alaska</option>
+                      	<option value="AZ">Arizona</option>
+                      	<option value="AR">Arkansas</option>
+                      	<option value="CA">California</option>
+                      	<option value="CO">Colorado</option>
+                      	<option value="CT">Connecticut</option>
+                      	<option value="DE">Delaware</option>
+                      	<option value="DC">District Of Columbia</option>
+                      	<option value="FL">Florida</option>
+                      	<option value="GA">Georgia</option>
+                      	<option value="HI">Hawaii</option>
+                      	<option value="ID">Idaho</option>
+                      	<option value="IL">Illinois</option>
+                      	<option value="IN">Indiana</option>
+                      	<option value="IA">Iowa</option>
+                      	<option value="KS">Kansas</option>
+                      	<option value="KY">Kentucky</option>
+                      	<option value="LA">Louisiana</option>
+                      	<option value="ME">Maine</option>
+                      	<option value="MD">Maryland</option>
+                      	<option value="MA">Massachusetts</option>
+                      	<option value="MI">Michigan</option>
+                      	<option value="MN">Minnesota</option>
+                      	<option value="MS">Mississippi</option>
+                      	<option value="MO">Missouri</option>
+                      	<option value="MT">Montana</option>
+                      	<option value="NE">Nebraska</option>
+                      	<option value="NV">Nevada</option>
+                      	<option value="NH">New Hampshire</option>
+                      	<option value="NJ">New Jersey</option>
+                      	<option value="NM">New Mexico</option>
+                      	<option value="NY">New York</option>
+                      	<option value="NC">North Carolina</option>
+                      	<option value="ND">North Dakota</option>
+                      	<option value="OH">Ohio</option>
+                      	<option value="OK">Oklahoma</option>
+                      	<option value="OR">Oregon</option>
+                      	<option value="PA">Pennsylvania</option>
+                      	<option value="RI">Rhode Island</option>
+                      	<option value="SC">South Carolina</option>
+                      	<option value="SD">South Dakota</option>
+                      	<option value="TN">Tennessee</option>
+                      	<option value="TX">Texas</option>
+                      	<option value="UT">Utah</option>
+                      	<option value="VT">Vermont</option>
+                      	<option value="VA">Virginia</option>
+                      	<option value="WA">Washington</option>
+                      	<option value="WV">West Virginia</option>
+                      	<option value="WI">Wisconsin</option>
+                      	<option value="WY">Wyoming</option>
+                      </select>
+                  </div>
+                   <div className="form-group">
+                      <label>Zip Code: </label>
+                      <input 
+                        type="text"
+                        id = "locationZip"
+                        className="form-control"
+                        value={this.state.locationZip}
+                        onChange={this.onChange}
+                        />
+                  </div>
+                  <div className="form-group">
+                      <input type="submit" value="Register Event" className="btn btn-primary"/>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+        </div>
+    );
+  }
+}
+
+CreateEvent.propTypes = {
+  addEvent: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { addEvent }
+)(withRouter(CreateEvent));
+
---
+++ server.js
@@ -0,0 +1,90 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const path = require("path");
+const request = require('request');
+const passport = require('passport');
+var moment = require('moment');
+
+const userRoute = require('./routes/api/users.js');
+const eventRoute = require('./routes/api/events.js');
+
+const port = process.env.PORT || 4000;
+const darkSkyAPI = process.env.darkSky || require("./config/keys").darkSkyAPI ;
+
+var NodeGeocoder = require('node-geocoder');
+var options = {
+  provider: 'opencage',
+  apiKey: require("./config/keys").openCageAPI || process.env.openCageAPI // for Mapquest, OpenCage, Google Premier
+};
+var geocoder = NodeGeocoder(options);
+
+const db = require("./config/keys").mongoURI;
+mongoose.set('useCreateIndex', true);
+mongoose.connect((process.env.MONGODB_URI || db), { useNewUrlParser: true }).then(
+  () => {console.log('Database is connected') },
+  err => { console.log('Can not connect to the database'+ err)}
+);
+
+// Passport middleware
+app.use(passport.initialize());
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use(cors());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './client/build')));
+
+app.post('/weather', (req, res) => {
+     
+    let lat = '';
+    let long = '';
+    let place = req.body.street + " " + req.body.city + ", " + req.body.state + " " + req.body.zip;
+    let time = moment(req.body.date + " " + req.body.time, 'dddd, MMMM Do YYYY h:mm A').unix();
+    
+    function getCoordinates(place) { 
+        return new Promise((resolve, reject) => {
+            geocoder.geocode(place, function(err,res) {
+                if(err)
+                    reject(err);
+                else
+                    resolve(res);
+                });
+        });
+    }
+    
+    (async () => {
+      const geoCoordinates = await getCoordinates(place);
+        lat = geoCoordinates[0].latitude;
+        long = geoCoordinates[0].longitude;
+        request('https://api.darksky.net/forecast/' + darkSkyAPI + '/' + lat +',' + long + ',' + time, function (error, response, body) {
+            const newBody = JSON.parse(body);
+            res.json(newBody);
+        });
+        lat = '';
+        long = '';
+    })();
+});
+
+app.use('/users', userRoute);
+app.use('/events', eventRoute);
+
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
+
+
+app.listen(port, () =>{
+  console.log('Server is running on Port:', port);
+});
+
+
+
+
+
+
