8d193234b0514799b7f237dc8c8730551c8de8fe
phantoms187 <warrend187@gmail.com>
2019-10-02 06:40:55 +0000

    Clean auth

---
+++ client/build/precache-manifest.78f1eaac8f93a61f84de802b83ad5376.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb59b93b6cfabc0d00bf828bbd808766",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4aa17e8a454ee4f9c48",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "3fdee33ca1058b35b922",
+    "url": "/static/css/main.bf2d2499.chunk.css"
+  },
+  {
+    "revision": "c4aa17e8a454ee4f9c48",
+    "url": "/static/js/2.4a3582ad.chunk.js"
+  },
+  {
+    "revision": "3fdee33ca1058b35b922",
+    "url": "/static/js/main.704ec10b.chunk.js"
+  },
+  {
+    "revision": "06b7cfbe5a7458d38821",
+    "url": "/static/js/runtime~main.1d872ddf.js"
+  }
+]);
