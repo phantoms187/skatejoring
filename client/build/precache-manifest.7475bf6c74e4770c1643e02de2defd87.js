abc31f3bd253dd4795db0d14fd00180d8994f999
phantoms187 <dwd2@pdx.edu>
2019-11-23 02:29:17 -0800

    maybe

---
+++ client/build/precache-manifest.7475bf6c74e4770c1643e02de2defd87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "362a69f4d863cd4deea1ab6e463640bf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f9053cd0e51917e8a6e",
+    "url": "/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "06e2d1336444247e3978",
+    "url": "/static/css/main.28847359.chunk.css"
+  },
+  {
+    "revision": "6f9053cd0e51917e8a6e",
+    "url": "/static/js/2.5fda06ca.chunk.js"
+  },
+  {
+    "revision": "06e2d1336444247e3978",
+    "url": "/static/js/main.13f167c4.chunk.js"
+  },
+  {
+    "revision": "06b7cfbe5a7458d38821",
+    "url": "/static/js/runtime~main.1d872ddf.js"
+  }
+]);
